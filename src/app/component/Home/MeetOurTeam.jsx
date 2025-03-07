e0ed8255abcea751afa41c435148f22832837403
MukterHossain <mukter3075@gmail.com>
2025-03-07 22:38:32 +0600

    added css in meetourteam

--- src/app/component/Home/MeetOurTeam.jsx
+++ src/app/component/Home/MeetOurTeam.jsx
@@ -3,7 +3,7 @@ import Heading from "@/app/hooks/Heading";
 
 const MeetOurTeam = () => {
     return (
-        <div>
+        <div className="mb-10">
             <Heading title={'Meet Our Team'} subtitle={'Team'} des={'the majority have suffered alteration in some form, by injected humour, or randomised words which dont look even slightly believable. '}></Heading>
         </div>
     );
